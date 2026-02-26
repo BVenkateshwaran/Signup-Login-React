54084adb161186c33077a022537557066f2600bb
BVenkateshwaran <bvenkateshwaran98@gmail.com>
2026-02-27 00:06:15 +0530

    Updated

--- src/Login.jsx
+++ src/Login.jsx
@@ -39,16 +39,18 @@ const Login = () => {
     <div>
 
       <h1>Login</h1>
-      
+
       <form action="">
         <label htmlFor="email">Email : </label>
         <input type="email" id="email" value={state.email} onChange={(e)=>dispatch({type:"email",payload:e.target.value})}/>
         <label htmlFor="password">Password : </label>
         <input type="password" id="password" value={state.password} onChange={(e)=>dispatch({type:"password",payload:e.target.value})}/>
+
         <button onClick={(e)=>{
           e.preventDefault()
           dispatch({type:"submit",payload:state})
         }}>Login</button>
+        
         <button onClick={(e)=>{
           e.preventDefault()
           dispatch({type:"reset"})
